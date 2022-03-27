aeec08a1e9115eca1bbf4d7158e8786033e25789
daniel <pinkdaniel@web.de>
2022-03-27 11:54:06 +0200

    simplify cam

--- N420_app/cam.py
+++ N420_app/cam.py
@@ -6,27 +6,26 @@ from pathlib import Path
 import os
 from threading import Thread
 
-class Cam(object):
+
+class Cam():
     capture_interval = timedelta(minutes=10)
-    last_capture = datetime.now()- capture_interval
+    last_capture = datetime.now() - capture_interval
     path = "data\\pictures"
 
     cam = cv2.VideoCapture(0)
+
     def __init__(self):
         Thread(target=self.camptureLoop).start()
-        
 
-    @classmethod
-    def safe_frame(cls, frame):
-        path = cls.path
+    def safe_frame(self, frame):
+        path = self.path
         Path("data\\pictures").mkdir(parents=True, exist_ok=True)
         name = datetime.now().strftime("%m-%d-%Y_%H-%M-%S") + ".png"
         path = os.path.join(path, name)
         print(path)
-        cv2.imwrite(path,frame)
+        cv2.imwrite(path, frame)
 
-    @classmethod
-    def camptureLoop(cls):
+    def camptureLoop(self):
         print('starting loop')
         while True:
             success, frame = Cam.cam.read()  # read the cam frame
@@ -34,12 +33,11 @@ class Cam(object):
                 pass
             else:
                 ret, buffer = cv2.imencode('.jpg', frame)
-                cls.frame = buffer.tobytes()
+                self.frame = buffer.tobytes()
 
-            if datetime.now() >= cls.last_capture + cls.capture_interval: 
-                cls.safe_frame(frame)
-                cls.last_capture = datetime.now()
-                   
-    @classmethod
-    def get_frame(cls):        
-        return cls.frame
+            if datetime.now() >= self.last_capture + self.capture_interval:
+                self.safe_frame(frame)
+                self.last_capture = datetime.now()
+
+    def get_frame(self):
+        return self.frame
